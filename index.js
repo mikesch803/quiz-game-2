1016b7420249033dfcd42040252890409157bfd5
Mahendra Chauhan <mikesch803@gmail.com>
2020-12-03 12:47:56 +0000

    added chalk

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-
 var readlineSync = require('readline-sync');
+const chalk = require('chalk');
 
 var score = 0
 
-var userName = readlineSync.question('Whats ur name? ')
+var userName = readlineSync.question(chalk.red('Whats ur name? '))
 
 console.log()
 
-console.log('Welcome! ' +userName.toUpperCase()+ ' Please answer given questions ')
-
-console.log()
-
-console.log('The questions are from famous Tarak mehta ka oolta chasma ')
+console.log(chalk.green('Welcome! ' +userName.toUpperCase()+ '. Please answer given questions which having two options. \n' +
+'The questions are from famous Tarak mehta ka oolta chasma '))
 
 console.log()
 
 function quizGame(question, answer) {
   
-var userAnswer = readlineSync.question(question+ ' OR enter e to exit!')
+var userAnswer = readlineSync.question(question+ ' OR enter e to exit!\n')
   if( userAnswer.toUpperCase() === answer.toUpperCase())
   {
-    console.log('you are right')
+    console.log(chalk.green('  you are right'))
     score++;
-    console.log('current score is ',score)
+    console.log(chalk.bgGreen('  current score is ',score))
   }else if(userAnswer === "e")
   {
     return userAnswer
   }else
   {
-    console.log('you are wrong')
-    console.log('current score is ',score)
+    console.log(chalk.red('  you are wrong'))
+    console.log(chalk.bgRed('  current score is ',score))
   }
 
   console.log('*************************************************')
@@ -39,27 +36,27 @@ var userAnswer = readlineSync.question(question+ ' OR enter e to exit!')
 
 
   var questions = [ { 
-    question: "Jethalal's wife?  a: daya, b: anjali ",
+    question: chalk.yellow("1. Jethalal's wife?\n  a: daya, b: anjali"),
    
     answer: "a"
   },
   {
-    question: "Jethala's friend?  a: iyer, b: Tarak",
+    question: chalk.red("2. Jethala's friend?\n  a: iyer, b: Tarak"),
    
     answer: "b"
   },
   {
-    question: "who is leader of tapusena?  a: tapu, b: sonu",
+    question: chalk.red("3. who is leader of tapusena?\n  a: tapu, b: sonu"),
    
     answer: "a"
   },
   {
-     question: "who is powerful man?  a: sodi, b: popatlal",
+     question: chalk.red("4. who is powerful man?\n  a: sodi, b: popatlal"),
    
     answer: "a"
   },
   {
-     question: "who want to turn world upside down?  a: bhide, b: popatlal",
+     question: chalk.red("5. who want to turn world upside down?\n  a: bhide, b: popatlal"),
    
     answer: "b"
   }
@@ -75,7 +72,7 @@ do{
 while(userAnswer!=="e" && i<questions.length)
 
 if(score>=4){
-console.log('YAY! total score is ',score)
+console.log('WELLDONE!' ,userName.toUpperCase(), ' Total score is ',score)
 }else{
-  console.log('Try Again :( ', '\nTotal score is :' ,score)
+  console.log('Try Again :( ', userName.toUpperCase(),' \nTotal score is :' ,score)
 }
